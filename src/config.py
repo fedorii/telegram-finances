d5dcd7a890e4f03ef1f64f86dbc2cd5e91615d9d
nefedori <nefedorai@gmail.com>
2024-10-24 12:59:31 +0500

    added user_id and username

--- src/config.py
+++ src/config.py
@@ -4,5 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot_token = os.getenv("BOT_TOKEN")
-sheet_key = os.getenv("SHEET_KEY")
+bot_token=os.getenv('BOT_TOKEN')
+sheet_key=os.getenv('SHEET_KEY')
+user_id=os.getenv('USER_ID')
+username=os.getenv('USER_NAME')
