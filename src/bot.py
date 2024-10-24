202aa6e9439acde8f324d035cca5e3268989a2d7
nefedori <nefedorai@gmail.com>
2024-10-24 13:12:01 +0500

    ???

--- src/bot.py
+++ src/bot.py
@@ -7,39 +7,35 @@ from aiogram.fsm.state import State, StatesGroup
 
 import asyncio
 from datetime import datetime
+from functools import wraps
 
 import config
 import database
 
 
 db = database.Database()
-
-
 class StateMachine(StatesGroup):
     waiting_for_amount = State()
 
 
-async def check_user(message: Message):
-    user = {
-        'id': message.from_user.id,
-        'username': message.from_user.username,
-        'language': message.from_user.language_code
-    }
-    if not db.check_user(user['id']):
-        db.add_user(
-            telegram_id=user['id'],
-            username=user['username'],
-            language=user['language']
-        )
+def access_required(func):
+    @wraps(func)
+    async def wrapper(message: Message, *args, **kwargs):
+        user = {'id': message.from_user.id, 'username': message.from_user.username}
+        if user['id'] == int(config.user_id) and user['username'] == config.username:
+            return await func(message, *args, **kwargs)
+        else:
+            await message.answer('Access to the bot is denied')
+    return wrapper
 
-
+@access_required
 async def command_start(message: Message):
     await message.answer('''
 Welcome to Telegram Finances Bot!
 Type /add to add new expense.
 ''')
 
-
+@access_required
 async def command_help(message: Message):
     await message.answer('''                         
 I'm here to help you account your cash expenditures.
@@ -51,7 +47,7 @@ Here's a list of the available commands:
     /help - Show this help message
 ''')
     
-
+@access_required
 async def command_add(message: Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [
@@ -62,7 +58,7 @@ async def command_add(message: Message):
     ])
     await message.answer('Choose the currency:', reply_markup=keyboard)
 
-
+@access_required
 async def command_remove(message: Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [
@@ -72,13 +68,13 @@ async def command_remove(message: Message):
     ])
     await message.answer('What do you want to remove?', reply_markup=keyboard)
 
-
-async def command_see():
-    pass
-
+@access_required
+async def command_see(message: Message):
+    table = db.format_table()
+    await message.answer(f"<pre>{table}</pre>", parse_mode="HTML")
 
 async def callback_controller(callback: CallbackQuery, state: FSMContext):
-    if callback.data.startswith('currency'):
+    if callback.data.startswith('currency_'):
         currency = callback.data.split('_')[1]
         await state.update_data(currency=currency)
         keyboard = InlineKeyboardMarkup(inline_keyboard=[[
@@ -98,16 +94,15 @@ async def callback_controller(callback: CallbackQuery, state: FSMContext):
             InlineKeyboardButton(text='Investments', callback_data='category_investments'),
         ]])
         await callback.message.answer('Please choose the category:', reply_markup=keyboard)
-    if callback.data.startswith('category'):
+    if callback.data.startswith('category_'):
         category = callback.data.split('_')[1]
         await state.update_data(category=category)
         await callback.message.answer('Please enter the expense in this form: {amount} {description}')
         await state.set_state(StateMachine.waiting_for_amount)
-    if callback.data.startswith('remove'):
+    if callback.data.startswith('remove_'):
         command = callback.data.split('_')[1]
         db.remove_expense(command)
         await callback.message.answer('Information has been removed')
-
 
 async def entry_amount(message: Message, state: FSMContext):
     user_data = await state.get_data()
@@ -126,7 +121,6 @@ async def entry_amount(message: Message, state: FSMContext):
     except ValueError:
         await message.answer('Invalid format. Please try again: {amount} {description}')
 
-
 async def main():
     bot = Bot(config.bot_token)
     dp = Dispatcher()
@@ -143,4 +137,7 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('bot disabled')
