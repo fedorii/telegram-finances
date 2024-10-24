020df9a43b53969b03fd680f4a4e53c021b8c929
nefedori <nefedorai@gmail.com>
2024-10-24 21:58:59 +0500

    added creds_file_path

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 bot_token=os.getenv('BOT_TOKEN')
-sheet_key=os.getenv('SHEET_KEY')
 user_id=os.getenv('USER_ID')
 username=os.getenv('USER_NAME')
+
+creds_file_path='credentials.json'
+sheet_key=os.getenv('SHEET_KEY')
